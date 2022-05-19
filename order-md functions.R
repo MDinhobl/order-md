38b6f03949db29208db2079f2956bb9756ede4d0
Mark W Dinhobl <103464896+MDinhobl@users.noreply.github.com>
2022-05-19 17:57:12 -0500

    Update order-md functions.R
    Increased computational speed and accuracy as well as ability to modify in the future. Thanks!

--- order-md functions.R
+++ order-md functions.R
@@ -1,251 +1,144 @@
 require(dplyr)
 
-
-order_md_score <- function(dataadjusted, ninput, noutput, xi, yi, M = 25, B = 30){
-  effinput <- 0
-  effoutput <- 0
-  effworst <- 0
-  effbest <- 0
-  xik <- 0
-  yik <- 0
-  for (j in 1:B){
-    
-    MSample = sample_n(dataadjusted, size = M, replace = TRUE)
-    
-    #INPUT
-    for (k in 1:ninput){
-      if (k == 1){
-        xik <- MSample[,k]/xi[k]
-      }else{
-        xik <- cbind(xik, MSample[,k]/xi[k])
-      }
-    }
-    xik <- as.data.frame(xik)
-    xik$bestxik <- apply(xik[1:ninput], MARGIN = 1, FUN = max, na.rm = TRUE)
-    
-    #OUTPUT
-    for (k in 1:noutput){
-      if (k == 1){
-        yik <- yi[k]/MSample[,(ninput+k)]
-      }else{
-        yik <- cbind(yik, yi[k]/MSample[,(ninput+k)])
-      }
-    }
-    yik <- as.data.frame(yik)
-    yik$bestyik <- apply(yik[1:noutput], MARGIN = 1, FUN = max, na.rm = TRUE)
-    
-    scores <- data.frame(xik,yik)
-    
-    effoutput[j] <- min(yik$best)
-    effinput[j] <- min(xik$best)
-    effworst[j] <- min(min(yik$best),min(xik$best))
-    effbest[j] <- max(min(yik$best),min(xik$best))
-  }
-  
-  
-  score_output <- mean(effoutput)
-  score_input <- mean(effinput)
-  score_worst <- mean(effworst)
-  score_best <- mean(effbest)
-  
-  return <- data.frame(score_input,score_output,score_worst,score_best)
-}
-order_md_conditional_score <- function(dataadjusted, ninput, noutput, xi, yi, M = 25, B = 30, probs){
-  effinput <- 0
-  effoutput <- 0
-  effworst <- 0
-  effbest <- 0
-  xik <- 0
-  yik <- 0
-  for (j in 1:B){
-    
-    MSample = sample_n(dataadjusted, size = M, replace = TRUE, weight = as.matrix(probs))
-    sample_n
-    #INPUT
-    for (k in 1:ninput){
-      if (k == 1){
-        xik <- MSample[,k]/xi[k]
-      }else{
-        xik <- cbind(xik, MSample[,k]/xi[k])
-      }
-    }
-    xik <- as.data.frame(xik)
-    xik$bestxik <- apply(xik[1:ninput], MARGIN = 1, FUN = max, na.rm = TRUE)
-    
-    #OUTPUT
-    for (k in 1:noutput){
-      if (k == 1){
-        yik <- yi[k]/MSample[,(ninput+k)]
-      }else{
-        yik <- cbind(yik, yi[k]/MSample[,(ninput+k)])
-      }
-    }
-    yik <- as.data.frame(yik)
-    yik$bestyik <- apply(yik[1:noutput], MARGIN = 1, FUN = max, na.rm = TRUE)
-    
-    scores <- data.frame(xik,yik)
-    
-    effoutput[j] <- min(yik$best)
-    effinput[j] <- min(xik$best)
-    effworst[j] <- min(min(yik$best),min(xik$best))
-    effbest[j] <- max(min(yik$best),min(xik$best))
-  }
-  
-  
-  conditional_score_output <- mean(effoutput)
-  conditional_score_input <- mean(effinput)
-  conditional_score_worst <- mean(effworst)
-  conditional_score_best <- mean(effbest)
-  
-  return <- data.frame(conditional_score_input,conditional_score_output,conditional_score_worst,conditional_score_best)
-}
-Quadrant_Split <- function(INPUT = INPUT, OUTPUT = OUTPUT, i = i){
-  
-  ninput <- ncol(INPUT)
-  noutput <- ncol(OUTPUT)
-  
-  xi <- INPUT[i,]
-  yi <- OUTPUT[i,]
-  
-  allygreater <- 1
-  for(j in 1:noutput){
-    allygreater <- ifelse(yi[j] <= OUTPUT[,j], allygreater, 0)
-  }
-  
-  allylesser <- 1
-  for(j in 1:noutput){
-    allylesser <- ifelse(yi[j] >= OUTPUT[,j], allylesser, 0)
-  }
-  
-  allxgreater <- 1
-  for(j in 1:noutput){
-    allxgreater <- ifelse(xi[j] <= INPUT[,j], allxgreater, 0)
-  }
-  
-  allxlesser <- 1
-  for(j in 1:noutput){
-    allxlesser <- ifelse(xi[j] >= INPUT[,j], allxlesser, 0)
-  }
-  
-  allygreater <- data.frame(allygreater)
-  allylesser <- data.frame(allylesser)
-  allxgreater <- data.frame(allxgreater)
-  allxlesser <- data.frame(allxlesser)
-  
-  returns <- cbind(allygreater, allylesser, allxgreater, allxlesser)
-}
-order_md <- function(ID, INPUT, OUTPUT, CONDITIONAL = NULL, DISCRETE = NULL, M = 25, B = 30, hlevel = .3, LOG = FALSE, onlyscores = TRUE){
+order_md <- function(INPUT, OUTPUT, CONDITIONAL = NULL, DISCRETE = NULL, M = 25, B = 100, hlevel = .1, LOG = FALSE, ConditionalControl = FALSE){
   #This function comes from the work primarily put forth by Simar, Wilson, and Darario. 
   #For a deeper look into the order-m and conditional order-m functions, read "Conditional nonparametric frontier models for convex and
   #nonconvex technologies: a unifying approach" by Cinzia Daraio and Leopold Simar (2007)
+  if(is.null(DISCRETE) == TRUE & is.null(CONDITIONAL) == TRUE){
+    probdone = FALSE
+  }else{probdone = TRUE}
   
-  h <- 0; compareable <- 0
-  
-  N <- nrow(ID)
-  
-  ninput <- ncol(INPUT)
-  noutput <- ncol(OUTPUT)
-  
-  data <- cbind(INPUT, OUTPUT)
-  
-  if(is.null(CONDITIONAL) == FALSE){
-    nconditional <- ncol(CONDITIONAL)
-    data <- cbind(data, CONDITIONAL)
-  }
   if(is.null(DISCRETE) == FALSE){
-    ndiscrete <- ncol(DISCRETE)
-    data <- cbind(data, DISCRETE)
+    DISCRETE <- data.frame(DISCRETE)
+    DISCRETE <- as.data.frame(lapply(DISCRETE, as.factor))
+    DISCRETE <- data.frame(lapply(DISCRETE, as.numeric))
+    DISCRETE <- data.matrix(DISCRETE)
   }
   
+  N <- nrow(INPUT)
   for (i in 1:N){
-    xi <- INPUT[i,]
-    yi <- OUTPUT[i,]
-    
-    SPLIT <- Quadrant_Split(INPUT = INPUT, OUTPUT = OUTPUT, i = i)
-    datatemp <- as.data.frame(cbind(data,SPLIT))
-    
+    start_time <- Sys.time()
+    datatemp <- Quadrant_Split(INPUT = INPUT, OUTPUT = OUTPUT, i = i)
     datatemp$probs <- 1
-    
-    
-    
     if(is.null(CONDITIONAL) == FALSE){
-      zi <- CONDITIONAL[i,]
-      probconditional <- probconditional(CONDITIONAL, zi, hlevel=.3, LOG = FALSE)
-      datatemp$probs <- datatemp$probs*probconditional
+      datatemp$probs <- probconditional(CONDITIONAL, i = i, hlevel = hlevel, LOG = LOG, ConditionalControl = ConditionalControl)*datatemp$probs
     }
     
     if(is.null(DISCRETE) == FALSE){
-      zidiscrete <- DISCRETE[i,]
-      probdiscrete <- probdiscrete(DISCRETE, zidiscrete)
-      datatemp$probs <- datatemp$probs*probdiscrete
+      datatemp$probs <- probdiscrete(DISCRETE, i=i)*datatemp$probs
     }
     
-    datatemp2 <- datatemp[-i,]
-    dataadjusted1 <- subset(datatemp2, allygreater == 1 & allxlesser == 1)
-    dataadjusted2 <- subset(datatemp2, allylesser == 1 & allxgreater == 1)
-    dataadjusted <- rbind(datatemp[i,],dataadjusted1,dataadjusted2)
-    
-    return_test <- order_md_score(dataadjusted,ninput,noutput,xi,yi, M, B)
-    
-    if(is.null(CONDITIONAL) == FALSE || is.null(DISCRETE) == FALSE){
-      probability <- dataadjusted$probs
-      return_test_2 <- order_md_conditional_score(dataadjusted, ninput, noutput, xi, yi, M, B, probs = probability)
-      return_test <- cbind(return_test,return_test_2)
-    }
+    dataadjusted <- datatemp %>% filter((xsmaller == TRUE & ylarger == TRUE)|(xlarger == TRUE & ysmaller == TRUE))
+    return_test <- order_md_score(dataadjusted, M = M, B = B, probdone = probdone)
     
     if(i == 1){
       scores <- return_test
     }else{
       scores <- rbind(scores, return_test)
     }
+    end_time <- Sys.time()
+    print(end_time - start_time)
+    print(i)
   }
-  if(onlyscores == TRUE){
-    return <- scores
-  }else{
-    return <- cbind(ID, data, scores)
-  }
-  return <- return
+  return <- data.frame(scores)
 }
-probconditional <- function(Z, zi, hlevel=.3, LOG = FALSE){
-  zi <- Z[i,]
-  nconditional <- ncol(as.data.frame(Z))
+order_m_input <- function(INPUT, OUTPUT, CONDITIONAL = NULL, DISCRETE = NULL, M = 25, B = 100, hlevel = .1, LOG = FALSE, ConditionalControl = FALSE){
+  #This function comes from the work primarily put forth by Simar, Wilson, and Darario. 
+  #For a deeper look into the order-m and conditional order-m functions, read "Conditional nonparametric frontier models for convex and
+  #nonconvex technologies: a unifying approach" by Cinzia Daraio and Leopold Simar (2007)
+  if(is.null(DISCRETE) == TRUE & is.null(CONDITIONAL) == TRUE){
+    probdone = FALSE
+  }else{probdone = TRUE}
   
-  if(LOG == TRUE){
-    for(j in 1:nconditional){
-      if(j==1){
-        difference <- Z[,j]-zi[j]
-        h <- hlevel
-        probs <- (1/sqrt(2*pi))*exp(-(abs(difference/h)/2))
-      }else{
-        difference <- Z[,j]-zi[j]
-        h <- hlevel
-        probs <- probs*(1/sqrt(2*pi))*exp(-(abs(difference/h)/2))
-      }
+  if(is.null(DISCRETE) == FALSE){
+    DISCRETE <- data.frame(DISCRETE)
+    DISCRETE <- as.data.frame(lapply(DISCRETE, as.factor))
+    DISCRETE <- data.frame(lapply(DISCRETE, as.numeric))
+    DISCRETE <- data.matrix(DISCRETE)
+  }
+  
+  N <- nrow(INPUT)
+  for (i in 1:N){
+    start_time <- Sys.time()
+    datatemp <- Quadrant_Split(INPUT = INPUT, OUTPUT = OUTPUT, i = i)
+    datatemp$probs <- 1
+    if(is.null(CONDITIONAL) == FALSE){
+      datatemp$probs <- probconditional(CONDITIONAL, i = i, hlevel = hlevel, LOG = LOG, ConditionalControl = ConditionalControl)*datatemp$probs
     }
+    
+    if(is.null(DISCRETE) == FALSE){
+      datatemp$probs <- probdiscrete(DISCRETE, i=i)*datatemp$probs
+    }
+    
+    dataadjusted <- datatemp %>% filter(ylarger == TRUE)
+    return_test <- order_md_score(dataadjusted, M = M, B = B, probdone = probdone)
+    
+    if(i == 1){
+      scores <- return_test
+    }else{
+      scores <- rbind(scores, return_test)
+    }
+    end_time <- Sys.time()
+    print(end_time - start_time)
+    print(i)
+  }
+  return <- data.frame(scores)
+}
+probconditional <- function(CONDITIONAL = CONDITIONAL, i = i, hlevel=.1, LOG = FALSE, ConditionalControl = FALSE){
+  Differences <- data.frame(sweep(CONDITIONAL, MARGIN = 2, STATS = CONDITIONAL[i,], '-'))
+  if (LOG == TRUE){
+    h <- hlevel
+    Differences <- Differences/h
   }else{
-    for(j in 1:nconditional){
-      if(j==1){
-        difference <- Z[,j]-zi[j]
-        h <- zi[j]*hlevel
-        probs <- (1/sqrt(2*pi))*exp(-(abs(difference/h)/2))
-      }else{
-        difference <- Z[,j]-zi[j]
-        h <- zi[j]*hlevel
-        probs <- probs*(1/sqrt(2*pi))*exp(-(abs(difference/h)/2))
-      }
-    }
+    Differences <- data.frame(sweep(Differences, MARGIN = 2, STATS = CONDITIONAL[i,]*hlevel, '/'))
+  }
+  probs <- exp(-(abs(Differences)/2))
+  probs <- apply(probs,1, prod)
+  if(ConditionalControl == TRUE){
+    probs <- probs^(1/ncol(CONDITIONAL))
   }
   returns <- as.data.frame(probs)
 }
-probdiscrete <- function(Zdiscrete, zidiscrete){
-  zidiscrete <- Zdiscrete[i,]
-  nconditionaldiscrete <- ncol(as.data.frame(Zdiscrete))
-  for(j in 1:nconditionaldiscrete){
-    if(j==1){
-      probs <- ifelse(Zdiscrete[,j] == zidiscrete[j], 1, 0)
-    }else{
-      probs <- ifelse(Zdiscrete[,j] == zidiscrete[j], probs, 0)
-    }
-  }
+Quadrant_Split <- function(INPUT = INPUT, OUTPUT = OUTPUT, i = i){
+  INPUTnew <- data.frame(sweep(INPUT, MARGIN = 2, STATS = INPUT[i,], '/'))
+  xik <- apply(INPUTnew, 1, max)
+  xlarger <- apply(INPUTnew >= 1, 1, all)
+  xsmaller <- apply(INPUTnew <= 1, 1, all)
+  
+  OUTPUTnew <- data.frame(sweep(OUTPUT, MARGIN = 2, STATS = OUTPUT[i,], '/'))
+  yik <- 1/apply(OUTPUTnew, 1, min)
+  ylarger <- apply(OUTPUTnew >= 1, 1, all)
+  ysmaller <- apply(OUTPUTnew <= 1, 1, all)
+  returns <- data.frame(xik,xlarger,xsmaller,yik,ylarger,ysmaller)
+}
+probdiscrete <- function(DISCRETE = DISCRETE, i = i){
+  Differences <- sweep(DISCRETE, MARGIN = 2, STATS = DISCRETE[i,], '-')
+  probs <- as.numeric(apply(Differences == 0, 1, all))
   returns <- as.data.frame(probs)
+}
+order_md_score <- function(dataadjusted, M = 25, B = 30, probdone = FALSE){
+  effinput <- 0
+  effoutput <- 0
+  effworst <- 0
+  effbest <- 0
+  for (j in 1:B){
+    if(probdone == FALSE){
+      MSample = slice_sample(dataadjusted, n = M, replace = TRUE)
+    }else{
+      MSample = slice_sample(dataadjusted, n = M, weight_by = as.matrix(dataadjusted$probs), replace = TRUE)
+    }
+    effinput[j] <- min(MSample$xik)
+    effoutput[j] <- min(MSample$yik)
+    effworst[j] <- min(min(MSample$yik),min(MSample$xik))
+    effbest[j] <- max(min(MSample$yik),min(MSample$xik))
+  }
+  score_input <- mean(effinput)
+  sd_input <- sd(effinput)
+  score_output <- mean(effoutput)
+  sd_output <- sd(effoutput)
+  score_best <- mean(effworst)
+  sd_best <- sd(effworst)
+  score_worst <- mean(effbest)
+  sd_worst <- sd(effbest)
+  return <- cbind(score_input, sd_input, score_output, sd_output, score_best, sd_best, score_worst, sd_worst)
 }
